5eb9909943e6b06ffe12620946007634552b440d
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2024-12-01 17:43:18 +0100

    refactor: npc id identification

--- npc-namespace/npc-namespace-citizens/src/main/kotlin/app/simplecloud/npc/namespace/citizens/CitizensNamespace.kt
+++ npc-namespace/npc-namespace-citizens/src/main/kotlin/app/simplecloud/npc/namespace/citizens/CitizensNamespace.kt
@@ -1,7 +1,11 @@
 package app.simplecloud.npc.namespace.citizens
 
-import app.simplecloud.npc.namespace.citizens.listener.*
+import app.simplecloud.npc.namespace.citizens.listener.NpcRemoveListener
+import app.simplecloud.npc.namespace.citizens.listener.PlayerCreateNpcListener
+import app.simplecloud.npc.namespace.citizens.listener.action.NpcLeftClickListener
+import app.simplecloud.npc.namespace.citizens.listener.action.NpcRightClickListener
 import app.simplecloud.npc.shared.namespace.NpcNamespace
+import net.citizensnpcs.api.npc.NPC
 import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.PluginManager
 
@@ -9,8 +13,9 @@ import org.bukkit.plugin.PluginManager
  * @author Niklas Nieberler
  */
 
-class CitizensNamespace : NpcNamespace(
-    "Citizens"
+class CitizensNamespace : NpcNamespace<NPC>(
+    "Citizens",
+    { it.id.toString() }
 ) {
 
     override fun onEnable() {
@@ -20,6 +25,9 @@ class CitizensNamespace : NpcNamespace(
     override fun registerListeners(pluginManager: PluginManager, plugin: Plugin) {
         pluginManager.registerEvents(NpcRightClickListener(this), plugin)
         pluginManager.registerEvents(NpcLeftClickListener(this), plugin)
+
+        pluginManager.registerEvents(PlayerCreateNpcListener(this), plugin)
+        pluginManager.registerEvents(NpcRemoveListener(this), plugin)
     }
 
 }
--- npc-shared/src/main/kotlin/app/simplecloud/npc/shared/namespace/NpcNamespace.kt
+++ npc-shared/src/main/kotlin/app/simplecloud/npc/shared/namespace/NpcNamespace.kt
@@ -1,6 +1,7 @@
 package app.simplecloud.npc.shared.namespace
 
 import app.simplecloud.npc.shared.action.interaction.InteractionExecutor
+import app.simplecloud.npc.shared.manager.NpcManager
 import app.simplecloud.npc.shared.repository.NpcRepository
 import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.PluginManager
@@ -12,14 +13,25 @@ import org.bukkit.plugin.PluginManager
 /**
  * This is a separate space for the loaded npc plugin on the server
  * @param pluginName will be loaded if this plugin is installed
+ * @param npcIdFunction find out the matching npc id
  */
-abstract class NpcNamespace(
-    val pluginName: String
+abstract class NpcNamespace<N : Any>(
+    val pluginName: String,
+    private val npcIdFunction: (N) -> String
 ) {
 
     val npcRepository = NpcRepository()
 
-    val interactionExecutor = InteractionExecutor(npcRepository)
+    val npcManager = NpcManager(this)
+    val interactionExecutor = InteractionExecutor(this)
+
+    /**
+     * Gets the matching id from the npc
+     * @return id of the npc
+     */
+    fun invokeNpcId(npc: N): String {
+        return this.npcIdFunction(npc)
+    }
 
     /**
      * Is executed when namespace must be loaded
