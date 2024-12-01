cf1ae6251b9607cc4c709ab6b90fceb8827ee34c
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2024-12-01 17:44:01 +0100

    refactor: npc id identification in InteractionExecutor

--- npc-shared/src/main/kotlin/app/simplecloud/npc/shared/action/interaction/InteractionExecutor.kt
+++ npc-shared/src/main/kotlin/app/simplecloud/npc/shared/action/interaction/InteractionExecutor.kt
@@ -1,23 +1,28 @@
 package app.simplecloud.npc.shared.action.interaction
 
+import app.simplecloud.npc.shared.namespace.NpcNamespace
 import app.simplecloud.npc.shared.option.Option
 import app.simplecloud.npc.shared.option.OptionProvider
-import app.simplecloud.npc.shared.repository.NpcRepository
 import org.bukkit.entity.Player
 
 /**
  * @author Niklas Nieberler
  */
 
-class InteractionExecutor(
-    private val repository: NpcRepository
+class InteractionExecutor<N : Any>(
+    private val namespace: NpcNamespace<N>
 ) {
 
     /**
-     *
+     * Executes the appropriate action from the [PlayerInteraction] for this npc
+     * @param npc this npc
+     * @param player the action executor
+     * @param playerInteraction the action type
+     * @param optionProvider the possible options
      */
-    fun execute(id: String, player: Player, playerInteraction: PlayerInteraction, optionProvider: OptionProvider = OptionProvider()) {
-        val config = this.repository.get(id) ?: return
+    fun execute(npc: N, player: Player, playerInteraction: PlayerInteraction, optionProvider: OptionProvider = OptionProvider()) {
+        val npcId = this.namespace.invokeNpcId(npc)
+        val config = this.namespace.npcRepository.get(npcId) ?: return
         val interaction = config.getPlayerInteraction(playerInteraction) ?: return
         optionProvider.add(
             *interaction.options.map { Option.of(it) }.toTypedArray(),
