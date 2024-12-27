7ddceb7b8dd5fc2ba2b95ee24fbb868a55867654
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2024-12-27 18:43:33 +0100

    feat: add release-dev.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,13 @@ plugins {
     alias(libs.plugins.shadow)
 }
 
+val baseVersion = "0.0.1"
+val commitHash = System.getenv("COMMIT_HASH")
+val snapshotVersion = "${baseVersion}-dev.$commitHash"
+
 allprojects {
     group = "app.simplecloud.plugin"
-    version = "1.0-SNAPSHOT"
+    version = if (commitHash != null) snapshotVersion else baseVersion
 
     repositories {
         mavenCentral()
