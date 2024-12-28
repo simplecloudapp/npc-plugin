386c1370ab93ca626cf1382dfd64fdf1575a7edb
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2024-12-28 20:47:08 +0100

    fix: hologram flicker in SpawnNpcActionHandler

--- npc-shared/src/main/kotlin/app/simplecloud/npc/shared/event/action/SpawnNpcActionHandler.kt
+++ npc-shared/src/main/kotlin/app/simplecloud/npc/shared/event/action/SpawnNpcActionHandler.kt
@@ -1,6 +1,9 @@
 package app.simplecloud.npc.shared.event.action
 
+import app.simplecloud.npc.shared.config.NpcConfig
 import app.simplecloud.npc.shared.event.EventActionHandler
+import app.simplecloud.npc.shared.hologram.HologramManager
+import app.simplecloud.npc.shared.hologram.HologramOptions
 import app.simplecloud.npc.shared.namespace.NpcNamespace
 import app.simplecloud.npc.shared.utils.Debouncer
 import kotlinx.coroutines.CoroutineScope
@@ -19,8 +22,21 @@ class SpawnNpcActionHandler : EventActionHandler {
         val npcConfig = namespace.npcRepository.get(id)
             ?: throw NullPointerException("failed to find npc $id")
         CoroutineScope(Dispatchers.IO).launch {
-            debouncer.debounce(this) { namespace.hologramManager.updateHolograms(npcConfig) }
+            debouncer.debounceSync(this) { updateHolograms(namespace.hologramManager, npcConfig) }
         }
+    }
+
+    private fun updateHolograms(
+        hologramManager: HologramManager,
+        npcConfig: NpcConfig
+    ) {
+        val coroutineScope = CoroutineScope(Dispatchers.IO)
+        if (hologramManager.getTextDisplays(npcConfig.id).isEmpty()) {
+            coroutineScope.launch { hologramManager.updateHolograms(npcConfig) }
+            return
+        }
+        val groupName = npcConfig.getOptionProvider().getOption(HologramOptions.PLACEHOLDER_GROUP_NAME)
+        coroutineScope.launch { hologramManager.updateTextHologramByGroup(npcConfig, groupName) }
     }
 
 }
