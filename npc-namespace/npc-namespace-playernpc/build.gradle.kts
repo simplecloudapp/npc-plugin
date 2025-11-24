9df0d9d4ced8da242723637b78d4bbc6362b267e
niklasnieberler <nikigalaxylp@gmail.com>
2025-11-24 08:51:35 +0100

    feat: remove player-npc support

--- npc-namespace/npc-namespace-playernpc/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-dependencies {
-    compileOnly(rootProject.libs.paperApi)
-    compileOnly(rootProject.libs.bundles.simpleCloudController)
-
-    compileOnly("dev.sergiferry:playernpc:2023.6")
-
-    implementation(project(":npc-shared"))
-}
--- npc-paper/build.gradle.kts
+++ npc-paper/build.gradle.kts
@@ -7,14 +7,13 @@ plugins {
 
 dependencies {
     compileOnly(rootProject.libs.paperApi)
-    implementation(rootProject.libs.bundles.simpleCloudController)
+    implementation(rootProject.libs.bundles.simpleCloud)
     implementation(rootProject.libs.bundles.cloudPaper)
     implementation(rootProject.libs.interfacesApi)
 
     implementation(project(":npc-shared"))
     implementation(project(":npc-namespace:npc-namespace-citizens"))
     implementation(project(":npc-namespace:npc-namespace-standalone"))
-    implementation(project(":npc-namespace:npc-namespace-playernpc"))
     implementation(project(":npc-namespace:npc-namespace-fancynpcs"))
     implementation(project(":npc-namespace:npc-namespace-mythicmobs"))
 }
