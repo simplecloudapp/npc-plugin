1de00025eea8ad6df874f1ba80bd2800c362e7b9
niklas.nieberler <niklas.nieberler@pollin.de>
2025-11-25 14:50:57 +0100

    ref: add configurate dependency in build.gradle.kts

--- npc-shared/build.gradle.kts
+++ npc-shared/build.gradle.kts
@@ -2,4 +2,6 @@ dependencies {
     compileOnly(rootProject.libs.paperApi)
     compileOnly(rootProject.libs.interfacesApi)
     compileOnly(rootProject.libs.bundles.simpleCloud)
+
+    compileOnly(rootProject.libs.configurate)
 }
